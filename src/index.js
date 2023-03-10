aa1652504d307ed10bfae0e6718e2544d3b7c86b
Akshara Sundararajan <aksharasun@Aksharas-MacBook-Pro.local>
2023-03-09 18:15:16 -0800

    updated prod uri

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://space-courses-api.herokuapp.com/", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-server-production-5d77.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
 });
 
 ReactDOM.render(
